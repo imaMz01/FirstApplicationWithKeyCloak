2f4d0264070fe88d65b2765cbb60af91b7dd08d9
imaMz01 <imanemzaouir66@gmail.com>
2024-09-21 16:06:00 +0100

    vesrion2

--- src/main/java/com/Peaqock/FirstAppWithKeyCloak/Config/SecurityConfiguration.java
+++ src/main/java/com/Peaqock/FirstAppWithKeyCloak/Config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.Peaqock.FirstAppWithKeyCloak.Config;
 
 
+import com.Peaqock.FirstAppWithKeyCloak.Converter.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,13 +10,16 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    private final JwtAuthConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws  Exception{
         httpSecurity
@@ -22,8 +27,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth->auth.anyRequest().authenticated());
 
         httpSecurity
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2 // It allows the application to accept and validate JWT tokens sent in requests with OAuth2 Resource Server
+                        .jwt(jwt->jwt.jwtAuthenticationConverter(jwtAuthConverter)) //we mention to spring that we want to use our converter instead to the default converter
                 );
 
         httpSecurity
--- src/main/java/com/Peaqock/FirstAppWithKeyCloak/Converter/JwtAuthConverter.java
+++ src/main/java/com/Peaqock/FirstAppWithKeyCloak/Converter/JwtAuthConverter.java
@@ -1,20 +1,69 @@
 package com.Peaqock.FirstAppWithKeyCloak.Converter;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 //we convert, because role should have a prefix "Role_", so we should add "Role_"
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter=new JwtGrantedAuthoritiesConverter();
+    @Value("${jwt.auth.converter.principle-attribute}")
+    private String pricipleAttribute;
+    @Value("${jwt.auth.converter.resource-id}")
+    private String resourceId;
     @Override
-    public AbstractAuthenticationToken convert(@NonNull Jwt source) {
-        return null;
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractRessourceRoles(jwt).stream()
+        )
+                .collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(jwt,authorities,getPrincipalClaimName(jwt));
+    }
+
+    public String getPrincipalClaimName(Jwt jwt){
+        String claimName = JwtClaimNames.SUB;
+        if(pricipleAttribute != null){
+            claimName=pricipleAttribute;
+        }
+        return jwt.getClaim(claimName);
+    }
+
+    public Collection< ? extends GrantedAuthority> extractRessourceRoles(Jwt jwt){
+        Map<String,Object> ressoursAccess;
+        Map<String,Object> ressource;
+        Collection<String> ressourceRole;
+
+        if(jwt.getClaim("resource_access") == null){
+            return Set.of();
+        }
+        ressoursAccess=jwt.getClaim("resource_access");
+
+        if(ressoursAccess.get(resourceId) == null){
+            return Set.of();
+        }
+        ressource= (Map<String, Object>) ressoursAccess.get(resourceId);
+        ressourceRole= (Collection<String>) ressource.get(("roles"));
+        return ressourceRole.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_"+role)).collect(Collectors.toSet());
     }
 }
